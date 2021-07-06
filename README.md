744947966757b76582d8286d89ca672c4c4a562d
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2021-07-06 15:17:00 +0700

    Write more classes and functions

---
+++ README.md
@@ -0,0 +1,14 @@
+# ImprovMX_PY Repository!
+Welcome to this repository!
+## Instruction
+Please install this through pip (Underdevelopment)
+```bash
+pip install improvmxpy
+```
+Or Clone this repo and install it by do
+```bash
+ git clone https://github.com/dumb-stuff/improvmx_py
+ cd improvmx_py
+ python  setup.py install
+ ```
+ this repo is development
--- main.py
+++ main.py
@@ -12,33 +12,33 @@ import json
 
 
 class WrongPatternToken(
-    Exception
+	Exception
 ):
-    pass
+	pass
 
 
 class NoToken(
-    Exception
+	Exception
 ):
-    pass
+	pass
 
 
 class TokenError(
-    Exception
+	Exception
 ):
-    pass
+	pass
 
 
 class NoPermission(
-    Exception
+	Exception
 ):
-    pass
+	pass
 
 
 class ServerError(
-    Exception
+	Exception
 ):
-    pass
+	pass
 
 
 url = "https://api.improvmx.com/v3/"
@@ -46,200 +46,212 @@ tokenauth = ""
 
 
 class SetUp(
-    object
+	object
 ):
-    def __init__(
-        self, 
-        token=None
-    ):
-        if len(
-            tokenauth
-        ) != 35 or len(
-        tokenauth
-    ) == 0:
-            print(
-                "Adding token"
-            )
-            if token is None or len(
-                token
-            ) == 0 or len(
-            token
-        ) != 35:
-                raise WrongPatternToken(
-                    "You have wrong token or You did'nt enter token."
-                )
-            else:
-                tokenauth = token
-        else:
-            print(
-                "You already setup token!"
-            )
+	def __init__(
+		self, 
+		token=None
+	):
+		if len(
+			tokenauth
+		) != 35 or len(
+		tokenauth
+	) == 0:
+			print(
+				"Adding token"
+			)
+			if token is None or len(
+				token
+			) == 0 or len(
+			token
+		) != 35:
+				raise WrongPatternToken(
+					"You have wrong token or You did'nt enter token."
+				)
+			tokenauth = token
+		else:
+			print(
+				"You already setup token!"
+			)
 
 
 class Account(
-    object
+	object
 ):
-    def __init__(
-        self
-    ):
-        if len(
-            tokenauth
-        ) != 35:
-            raise NoToken(
-                "We don't see any token that you input! You can do that by do call SetUp function"
-            )
-        self.token = tokenauth
+	def __init__(
+		self
+	):
+		if len(
+			tokenauth
+		) != 35:
+			raise NoToken(
+				"We don't see any token that you input! You can do that by do call SetUp function"
+			)
+		self.token = tokenauth
 
-    def GetAccountDetail(
-        self
-    ):
-        r = requests.get(
-            f"{url}account",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def GetAccountDetail(
+		self
+	):
+		r = requests.get(
+			f"{url}account",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
-    def GetWhiteLabelDomain(
-        self
-    ):
-        r = requests.get(
-            f"{url}account/whitelabels",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def GetWhiteLabelDomain(
+		self
+	):
+		r = requests.get(
+			f"{url}account/whitelabels",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
 
 class Domains(
-    object
+	object
 ):
-    def __init__(
-        self
-    ):
-        if len(
-            tokenauth
-        ) != 35:
-            raise NoToken(
-                "We don't see any token that you input! You can do that by call SetUp function!"
-            )
-        self.token = tokenauth
+	def __init__(
+		self
+	):
+		if len(
+			tokenauth
+		) != 35:
+			raise NoToken(
+				"We don't see any token that you input! You can do that by call SetUp function!"
+			)
+		self.token = tokenauth
 
-    def ListDomains(
-        self, 
-        query: str = "", 
-        is_active: bool = "", 
-        limit=50, 
-        page=1
-    ):
-        r = requests.get(
-            f"{url}domains?q={query}&is_active={is_active}&limit={limit}&page={page}",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def ListDomains(
+		self, 
+		query: str = "", 
+		is_active: bool = "", 
+		limit=50, 
+		page=1
+	):
+		r = requests.get(
+			f"{url}domains?q={query}&is_active={is_active}&limit={limit}&page={page}",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
-    def AddDomain(
-        self, 
-        domain: str, 
-        notify_email: str = "", 
-        whitelabel: str = ""
-    ):
-        r = requests.post(
-            f"{url}domains?domain={domain}&notification_email={notify_email}&whitelabel={whitelabel}",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def AddDomain(
+		self, 
+		domain: str, 
+		notify_email: str = "", 
+		whitelabel: str = ""
+	):
+		r = requests.post(
+			f"{url}domains?domain={domain}&notification_email={notify_email}&whitelabel={whitelabel}",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
-    def DomainDetail(
-        self, domain
-    ):
-        r = requests.get(
-            f"{url}domains/{domain}",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def DomainDetail(
+		self, domain
+	):
+		r = requests.get(
+			f"{url}domains/{domain}",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
-    def EditDomain(
-        self, 
-        domain:str, 
-        notify_email: str = "", 
-        whitelabel: str = ""
-    ):
-        r = requests.put(
-            f"{url}domains/{domain}?notification_email={notify_email}&whitelabel={whitelabel}",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def EditDomain(
+		self, 
+		domain:str, 
+		notify_email: str = "", 
+		whitelabel: str = ""
+	):
+		r = requests.put(
+			f"{url}domains/{domain}?notification_email={notify_email}&whitelabel={whitelabel}",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
-    def DeleteDomain(
-        self, domain
-    ):
-        r = requests.delete(
-            f"{url}domains/{domain}",
-            auth=("api", self.token)
-        )
-        if json.loads(
-            r.content.decode()
-        )["success"] == True:
-            return True
-        return False
-    def CheckMXDomain(
-        self, 
-        domain:str
-    ):
-        r = requests.get(
-            f"{url}domains/{domain}/check",
-            auth=("api", self.token)
-        )
-        CheckResponse(
-            r
-        )
+	def DeleteDomain(
+		self, domain
+	):
+		r = requests.delete(
+			f"{url}domains/{domain}",
+			auth=("api", self.token)
+		)
+		if json.loads(
+			r.content.decode()
+		)["success"] is True:
+			return True
+		return False
+	def CheckMXDomain(
+		self, 
+		domain:str
+	):
+		r = requests.get(
+			f"{url}domains/{domain}/check",
+			auth=("api", self.token)
+		)
+		CheckResponse(
+			r
+		)
 
 
 class Aliases(
-    object
+	object
 ):
-    def __init__(
-        self
-    ):
-        if len(
-            tokenauth
-        ) != 35:
-            raise NoToken(
-                "We don't see any token that you input! You can do that by call SetUp function!"
-            )
-        self.token = tokenauth
-
-
+	def __init__(
+		self
+	):
+		if len(
+			tokenauth
+		) != 35:
+			raise NoToken(
+				"We don't see any token that you input! You can do that by call SetUp function!"
+			)
+		self.token = tokenauth
+	def AliasDomainList(
+		self,
+		domain:str,
+		q:str = "",
+		is_active:bool = "",
+		page:str = "1"
+	):
+		r = requests.get(
+			f"{url}domains/{domain}/aliases?q={q}&is_active={is_active}&page={page}",
+			auth=('api',self.token)
+		)
+		CheckResponse(
+			r
+		)
+	
 class CheckResponse(
-    object
+	object
 ):
-    def __init__(
-        self, 
-        r
-    ):
-        if r.status_code == 401:
-            tokenauth = ""
-            raise TokenError("Your token is unusable! Please set new token again.")
-        if r.status_code == 403:
-            raise NoPermission(
-                "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
-            )
-        if r.status_code == 500:
-            raise ServerError(
-                "Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
-            )
-        return json.loads(
-            r.content.decode(
-            )
-        )
+	def __init__(
+		self, 
+		r
+	):
+		if r.status_code == 401:
+			tokenauth = ""
+			raise TokenError("Your token is unusable! Please set new token again.")
+		if r.status_code == 403:
+			raise NoPermission(
+				"You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
+			)
+		if r.status_code == 500:
+			raise ServerError(
+				"Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
+			)
+		return json.loads(
+			r.content.decode(
+			)
+		)
