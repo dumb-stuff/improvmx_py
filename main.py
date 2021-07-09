28913ca8340355ffd75478c42b5e696fa12e328b
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2021-07-09 15:48:06 +0700

    Added Virtual Enviroment (So that's why it so large xD) Also Done with async API and blocking API! WOOOOOOOOOOOOOoo

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ oops
 """
 import requests
 
-import aiohttp
-
 import json
 
 import sys
@@ -77,11 +75,26 @@ class Account(object):
     def GetAccountDetail(self):
         global tokenauth
         r = requests.get(f"{url}account", auth=("api", self.token))
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
     def GetWhiteLabelDomain(self):
         r = requests.get(f"{url}account/whitelabels", auth=("api", self.token))
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
+
+    def __CheckResponse(self, r):
+        global tokenauth
+        if r.status_code == 401:
+            tokenauth = ""
+            raise TokenError("Your token is unusable! Please set new token again.")
+        if r.status_code == 403:
+            raise NoPermission(
+                "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
+            )
+        if r.status_code == 500:
+            raise ServerError(
+                "Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
+            )
+        return json.loads(r.content.decode("utf-8"))
 
 
 class Domains(object):
@@ -98,25 +111,31 @@ class Domains(object):
             f"{url}domains?q={query}&is_active={is_active}&limit={limit}&page={page}",
             auth=("api", self.token),
         )
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
     def AddDomain(self, domain: str, notify_email: str = "", whitelabel: str = ""):
         r = requests.post(
-            f"{url}domains?domain={domain}&notification_email={notify_email}&whitelabel={whitelabel}",
+            f"{url}",
             auth=("api", self.token),
+            headers={
+                "domain": domain,
+                "notification_email": notify_email,
+                "whitelabel": whitelabel,
+            },
         )
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
     def DomainDetail(self, domain):
         r = requests.get(f"{url}domains/{domain}", auth=("api", self.token))
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
     def EditDomain(self, domain: str, notify_email: str = "", whitelabel: str = ""):
         r = requests.put(
-            f"{url}domains/{domain}?notification_email={notify_email}&whitelabel={whitelabel}",
+            f"{url}domains",
             auth=("api", self.token),
+            headers={"notification_email": notify_email, "whitelabel": whitelabel},
         )
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
     def DeleteDomain(self, domain):
         r = requests.delete(f"{url}domains/{domain}", auth=("api", self.token))
@@ -126,7 +145,22 @@ class Domains(object):
 
     def CheckMXDomain(self, domain: str):
         r = requests.get(f"{url}domains/{domain}/check", auth=("api", self.token))
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
+
+    def __CheckResponse(self, r):
+        global tokenauth
+        if r.status_code == 401:
+            tokenauth = ""
+            raise TokenError("Your token is unusable! Please set new token again.")
+        if r.status_code == 403:
+            raise NoPermission(
+                "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
+            )
+        if r.status_code == 500:
+            raise ServerError(
+                "Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
+            )
+        return json.loads(r.content.decode("utf-8"))
 
 
 class Aliases(object):
@@ -145,14 +179,41 @@ class Aliases(object):
             f"{url}domains/{domain}/aliases?q={q}&is_active={is_active}&page={page}",
             auth=("api", self.token),
         )
-        return CheckResponse(r)
+        return self.__CheckResponse(r)
 
-def CheckResponse(r):
+    def AddNewAlias(self, domain, alias, forward):
+        r = requests.post(
+            f"{url}{domain}/aliases",
+            auth=("api", self.token),
+            headers={"alias": alias, "forward": forward},
+        )
+        return self.__CheckResponse(r)
+
+    def GetDetailAlias(self, domain, alias):
+        r = requests.get(
+            f"{url}domains/{domain}/aliases/{alias}", auth=("api", self.token)
+        )
+        return self.__CheckResponse(r)
+
+    def UpdateAlias(self, domain, alias, forward):
+        r = requests.put(
+            f"{url}domains/{domain}/aliases/{alias}",
+            headers={"forward": forward},
+            auth=("api", self.token),
+        )
+        return self.__CheckResponse(r)
+
+    def DeleteAlias(self, domain, alias):
+        r = requests.delete(
+            f"{url}domains/{domain}/aliases/{alias}", auth=("api", self.token)
+        )
+        return self.__CheckResponse(r)
+
+    def __CheckResponse(self, r):
         global tokenauth
         if r.status_code == 401:
             tokenauth = ""
-            raise TokenError("Your token is unusable! Please set new token again."
-            )
+            raise TokenError("Your token is unusable! Please set new token again.")
         if r.status_code == 403:
             raise NoPermission(
                 "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
@@ -164,3 +225,101 @@ def CheckResponse(r):
         return json.loads(r.content.decode("utf-8"))
 
 
+class Logging(object):
+    def __init__(self):
+        global tokenauth
+        if len(tokenauth) != 35:
+            raise NoToken(
+                "We don't see any token that you input! You can do that by call SetUp function!"
+            )
+        self.token = tokenauth
+
+    def GetDomainLog(self, domain, logID=None):
+        if logID is not None:
+            r = requests.get(
+                f"{url}domains/{domain}/logs",
+                headers={"next_cursor": logID},
+                auth=("api", self.token),
+            )
+            return self.__CheckResponse(r)
+        r = requests.get(f"{url}domains/{domain}/logs", auth=("api", self.token))
+        return self.__CheckResponse(r)
+
+    def GetAliasLog(self, domain, alias, logID=None):
+        if logID is not None:
+            r = requests.get(
+                f"{url}domains/{domain}/logs/aliases/{alias}",
+                headers={"next_cursor": logID},
+                auth=("api", self.token),
+            )
+            return self.__CheckResponse(r)
+        r = requests.get(
+            f"{url}domains/{domain}/logs/aliases/{alias}", auth=("api", self.token)
+        )
+        return self.__CheckResponse(r)
+
+    def __CheckResponse(self, r):
+        global tokenauth
+        if r.status_code == 401:
+            tokenauth = ""
+            raise TokenError("Your token is unusable! Please set new token again.")
+        if r.status_code == 403:
+            raise NoPermission(
+                "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
+            )
+        if r.status_code == 500:
+            raise ServerError(
+                "Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
+            )
+        return json.loads(r.content.decode("utf-8"))
+
+
+class SMTPCredential(object):
+    def __init__(self):
+        global tokenauth
+        if len(tokenauth) != 35:
+            raise NoToken(
+                "We don't see any token that you input! You can do that by call SetUp function!"
+            )
+        self.token = tokenauth
+
+    def ListOfSMTPAccount(self, domain):
+        r = requests.get(f"{url}domains/{domain}/credentials", auth=("api", self.token))
+        return self.__CheckResponse(r)
+
+    def AddNewSMTPAccount(self, domain, username, password):
+        r = requests.post(
+            f"{url}domains/{domain}/credentials",
+            auth=("api", self.token),
+            headers={"username": username, "password": password},
+        )
+        return self.__CheckResponse(r)
+
+    def ChangeSMTPUserPassword(self, domain, username, password):
+        r = requests.put(
+            f"{url}domains/{domain}/credentials/{username}",
+            auth=("api", self.token),
+            headers={"password": password},
+        )
+        return self.__CheckResponse(r)
+
+    def DeleteSMTPUser(self, domain, username):
+        r = requests.delete(f"{url}domains/{domain}/credentials/{username}")
+        if json.loads(r.content.decode("utf-8"))["success"] == True:
+            return True
+        return False
+
+    def __CheckResponse(self, r):
+        global tokenauth
+        if r.status_code == 401:
+            tokenauth = ""
+            raise TokenError("Your token is unusable! Please set new token again.")
+        if r.status_code == 403:
+            raise NoPermission(
+                "You don't have enough permission to do this. You may need to subscribe pro or organization plan!"
+            )
+        if r.status_code == 500:
+            raise ServerError(
+                "Oops! The ImprovMX server is died or down or there's bug! Please try again later!"
+            )
+        return json.loads(r.content.decode("utf-8"))
